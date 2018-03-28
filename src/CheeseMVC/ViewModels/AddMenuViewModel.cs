fc9d468ae536a87165b9893b076857409ad54df7
samharden <samharden@gmail.com>
2018-03-28 01:05:51 -0400

    moar

---
+++ src/CheeseMVC/ViewModels/AddMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    // Part 3: Setting Up a Many-to-Many Relationship
+    public class AddMenuViewModel
+    {
+        [Required]
+        [Display(Name = "Menu Name")]
+        public string Name { get; set; }
+    }
+}
