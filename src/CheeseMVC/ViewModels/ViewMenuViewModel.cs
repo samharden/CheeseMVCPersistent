aadfd1354b626c30a35a9746548a245259424408
samharden <samharden@gmail.com>
2018-03-28 01:14:19 -0400

    BV

---
+++ src/CheeseMVC/ViewModels/ViewMenuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CheeseMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    // Part 3: Setting Up a Many-to-Many Relationship
+    public class ViewMenuViewModel
+    {
+        public Menu Menu { get; set; }
+
+        public IList<CheeseMenu> Items { get; set; }
+    }
+}
